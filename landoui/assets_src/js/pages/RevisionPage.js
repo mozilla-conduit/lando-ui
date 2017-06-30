82721bf251791e3ec9e3f517fcc578412bf80067
Israel Madueme <purelogiq@gmail.com>
2017-06-30 14:30:12 -0400

    Refactor static ui structure and display revisions

---
+++ landoui/assets_src/js/pages/RevisionPage.js
@@ -0,0 +1,25 @@
+$.fn.landoRevisionPage = function(){
+  return this.each(function(){
+    let $page = $(this);
+    let state = $page.data('state');
+    $page.find('.RevisionPage-collapseLink').landoRevisionPageCollapsible();
+  });
+};
+
+$.fn.landoRevisionPageCollapsible = function(){
+  const iconClassSelectors = '.fa-chevron-circle-right, .fa-chevron-circle-down';
+  const iconClassToggle = 'fa-chevron-circle-right, fa-chevron-circle-down';
+
+  return this.each(function(){
+    let $revision = $(this).closest('.RevisionPage-parentRevision').first();
+
+    $(this).on('click', function(){
+      $revision.find(iconClassSelectors).toggleClass(iconClassToggle);
+      $revision.find('.RevisionPage-parentRevisionBody').toggle(200);
+    });
+  });
+};
+
+$(document).ready(function () {
+  $('.RevisionPage').landoRevisionPage();
+});
