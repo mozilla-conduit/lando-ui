b4d236adbccbe01df1fb76bf4da711ef40d9dad1
Israel Madueme <purelogiq@gmail.com>
2017-07-11 12:27:44 -0400

    Cleanup javascript style and remove .gitkeep files

--- landoui/assets_src/js/pages/RevisionPage.js
+++ landoui/assets_src/js/pages/RevisionPage.js
@@ -1,25 +1,23 @@
-$.fn.landoRevisionPage = function(){
-  return this.each(function(){
+'use strict';
+
+$.fn.landoRevisionPage = function() {
+  return this.each(function() {
     let $page = $(this);
     let state = $page.data('state');
     $page.find('.RevisionPage-collapseLink').landoRevisionPageCollapsible();
   });
 };
 
-$.fn.landoRevisionPageCollapsible = function(){
+$.fn.landoRevisionPageCollapsible = function() {
   const iconClassSelectors = '.fa-chevron-circle-right, .fa-chevron-circle-down';
   const iconClassToggle = 'fa-chevron-circle-right, fa-chevron-circle-down';
 
-  return this.each(function(){
+  return this.each(function() {
     let $revision = $(this).closest('.RevisionPage-parentRevision').first();
 
-    $(this).on('click', function(){
+    $(this).on('click', function() {
       $revision.find(iconClassSelectors).toggleClass(iconClassToggle);
       $revision.find('.RevisionPage-parentRevisionBody').toggle(200);
     });
   });
 };
-
-$(document).ready(function () {
-  $('.RevisionPage').landoRevisionPage();
-});
