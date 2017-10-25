2695c5a4dc95aa541194758a86232ef9e24d66a7
David Walsh <davidwalsh83@gmail.com>
2017-10-25 10:47:01 -0500

    Fix the canned response to prevent build breakage
    Summary: Fix the canned response to prevent build breakage
    
    Test Plan: invoke test
    
    Reviewers: imadueme
    
    Reviewed By: imadueme
    
    Differential Revision: https://phabricator.services.mozilla.com/D163

--- tests/canned_responses.py
+++ tests/canned_responses.py
@@ -6,56 +6,42 @@
 
 LANDO_API_RESPONSE = {
     "author": {
-        "image_url": "https://d2kb8dptaglwte.cloudfront.net/file/data/oywgsrq6rtv5rdfbjvdv/PHID-FILE-632bsum6ksnpu77kymbq/alphanumeric_lato-dark_I.png-_5e622c-255%2C255%2C255%2C0.4.png",
-        "phid": "PHID-USER-zlfme42o3yewh5v4k6ry",
-        "real_name": "Israel Madueme",
-        "url": "https://mozphab.dev.mozaws.net/p/imadueme_admin/",
-        "username": "imadueme_admin"
+        "username": "dkl",
+        "url": "https://phabricator-dev.allizom.org/p/dkl/",
+        "image_url": "https://d1ro22lwo621zr.cloudfront.net/file/data/f7caskb6qt5gkxdkftlt/PHID-FILE-p2p6po32js7anduomkao/alphanumeric_aleo-white_D.png-_84c08c-0%2C0%2C0%2C0.png",
+        "real_name": "David Lawrence",
+        "phid": "PHID-USER-4wigy3sh5fc5t74vapwm"
     },
-    "bug_id": None,
-    "date_created": 1495217456,
-    "date_modified": 1498565006,
-    "id": 1,
-    "parent_revisions": [
-        {
-            "author": {
-                "image_url": "https://d2kb8dptaglwte.cloudfront.net/file/data/ifecwljgvarrzk2z3psj/PHID-FILE-2pj3vvuhi6gof6susyqu/alphanumeric_lato-dark_M.png-_bdb0f0-255%2C255%2C255%2C0.4.png",
-                "phid": "PHID-USER-uhfcvek26nuzv2wdf7yp",
-                "real_name": "Mark Cote",
-                "url": "https://mozphab.dev.mozaws.net/p/mcote_admin/",
-                "username": "mcote_admin"
-            },
-            "bug_id": None,
-            "date_created": 1496179554,
-            "date_modified": 1496697530,
-            "id": 5,
-            "parent_revisions": [],
-            "phid": "PHID-DREV-ixfdpmt54f5tz2gf6i7m",
-            "repo": {
-                "full_name": "rMOZILLACENTRAL mozilla-central",
-                "phid": "PHID-REPO-khtgyhvsqje2yylf2hzd",
-                "short_name": "rMOZILLACENTRAL",
-                "url": "https://mozphab.dev.mozaws.net/source/mozilla-central/"
-            },
-            "status": 0,
-            "status_name": "Needs Review",
-            "summary": "",
-            "test_plan": "",
-            "title": "Test manual rename.",
-            "url": "https://mozphab.dev.mozaws.net/D5"
-        }
-    ],
-    "phid": "PHID-DREV-kcobr5ckqlshytrnxnos",
-    "repo": {
-        "full_name": "rMOZILLACENTRAL mozilla-central",
-        "phid": "PHID-REPO-khtgyhvsqje2yylf2hzd",
-        "short_name": "rMOZILLACENTRAL",
-        "url": "https://mozphab.dev.mozaws.net/source/mozilla-central/"
-    },
-    "status": 0,
+    "date_modified": 1508942565,
     "status_name": "Needs Review",
-    "summary": "This is a diff",
-    "test_plan": "Look at it",
-    "title": "Bug 564987 - buildbot should cope when firefox/plugins doesn't exist when the config option is just right to trigger a super long revision name that can be used to test the ui",
-    "url": "https://mozphab.dev.mozaws.net/D1"
+    "date_created": 1504192226,
+    "url": "https://phabricator-dev.allizom.org/D3",
+    "id": 3,
+    "bug_id": 1154784,
+    "commit_message_preview": "Bug 1154784 - test",
+    "test_plan": "test",
+    "summary": "test",
+    "repo": {
+        "full_name": "rLANDOAPI lando-api",
+        "phid": "PHID-REPO-kzhprsvk5ju523p46bhg",
+        "short_name": "rLANDOAPI",
+        "url": "https://phabricator-dev.allizom.org/source/lando-api/"
+    },
+    "parent_revisions": [],
+    "title": "test",
+    "status": 0,
+    "phid": "PHID-DREV-hv7xoyupew3yie75nmlz",
+    "diff": {
+        "authors": [
+            {
+                "email": "pzalewa@mozilla.com",
+                "name": "Piotr Zalewa"
+            }
+        ],
+        "date_created": 1506951134,
+        "date_modified": 1506951137,
+        "id": 49,
+        "revision_id": 23,
+        "vcs_base_revision": "2cb40959b19124b2de7aea328073d542e283f4c0"
+    }
 }
