e5db59ac1c4525658675dcaea3da701d45ec2544
Mark Côté <mcote@mozilla.com>
2017-11-10 16:27:43 -0500

    Fix flake8 errors.

--- landoui/helpers.py
+++ landoui/helpers.py
@@ -7,21 +7,21 @@ from flask import (request, session)
 def set_last_local_referrer():
     """
     Sets the url of the last route that the user visited on this server.
-    
-    This is mainly used to implement our login flow: 
+
+    This is mainly used to implement our login flow:
         - Most pages are initially public (i.e. you do not have to sign in).
           This means they cannot be protected with the 'oidc_auth' decorator.
-        - Routes protected with the 'oidc_auth' decorator require login 
-          before any code in that route is executed. 
+        - Routes protected with the 'oidc_auth' decorator require login
+          before any code in that route is executed.
         - Going to a protected route will immediately redirect to Auth0.
-        - Upon successful login, flask-pyoidc will redirect back to the original
-          route that was decorated.
-    
+        - Upon successful login, flask-pyoidc will redirect back to the
+          original route that was decorated.
+
     Considering this we need a dedicated 'signin' route which will be protected
-    and when the user is redirected back to the route, it will then redirect 
+    and when the user is redirected back to the route, it will then redirect
     them to the last_local_referrer stored in their session.
     This referrer can of course be used for many other things.
-    
+
     This does not activate for the IGNORED_ROUTES defined inside this method.
     """
     IGNORED_ROUTES = ['/signin', '/signout']
--- landoui/revisions.py
+++ landoui/revisions.py
@@ -1,13 +1,9 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-import os
-
 import requests
 
-from flask import (
-    abort, Blueprint, current_app, jsonify, redirect, render_template, session
-)
+from flask import (abort, Blueprint, current_app, render_template)
 
 from landoui.helpers import set_last_local_referrer
 from landoui.sentry import sentry
