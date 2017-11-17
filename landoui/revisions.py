a8b620cd8ea8676b88deebf62f7498f561db2abf
David Walsh <davidwalsh83@gmail.com>
2017-11-17 12:39:40 -0600

    Make request for landing when Land button is clicked
    Summary: When the land button is clicked, a request is made to Lando API to land the revision and diff ID
    
    Reviewers: imadueme
    
    Reviewed By: imadueme
    
    Subscribers: zalun, smacleod
    
    Differential Revision: https://phabricator.services.mozilla.com/D257

--- landoui/revisions.py
+++ landoui/revisions.py
@@ -1,13 +1,18 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+import logging
 import requests
 
-from flask import (abort, Blueprint, current_app, render_template)
+from flask import (
+    abort, Blueprint, current_app, redirect, render_template, session
+)
 
 from landoui.forms import RevisionForm
 from landoui.helpers import set_last_local_referrer
 from landoui.sentry import sentry
+
+logger = logging.getLogger(__name__)
 
 revisions = Blueprint('revisions', __name__)
 revisions.before_request(set_last_local_referrer)
@@ -18,8 +23,8 @@ revisions.before_request(set_last_local_referrer)
 @revisions.route('/revisions/<revision_id>')
 def revision(revision_id, diff_id=''):
     # TODO:  Add diff ID when the API side is complete
-    revision_api_url = '{}/revisions/{}'.format(
-        current_app.config['LANDO_API_URL'], revision_id
+    revision_api_url = '{host}/revisions/{revision_id}'.format(
+        host=current_app.config['LANDO_API_URL'], revision_id=revision_id
     )
 
     try:
@@ -43,8 +48,36 @@ def revision(revision_id, diff_id=''):
     if form.is_submitted():
         if form.validate():
             # TODO
-            # Any more validation and then request to API
-            pass
+            # Any more basic validation
+
+            # Make request to API for landing
+            diff_id = int(form.diff_id.data)
+            land_response = requests.post(
+                '{host}/landings'.format(
+                    host=current_app.config['LANDO_API_URL']
+                ),
+                json={
+                    'revision_id': revision_id,
+                    'diff_id': diff_id,
+                },
+                headers={
+                    # TODO:  Add Phabricator API key for private revisions
+                    # 'X-Phabricator-API-Key': '',
+                    'Authorization':
+                    'Bearer {}'.format(session['access_token']),
+                    'Content-Type': 'application/json',
+                }
+            )
+            logger.info(land_response.json(), 'revision.landing.response')
+
+            if land_response.status_code == 200:
+                redirect_url = '/revisions/{revision_id}/{diff_id}'.format(
+                    revision_id=revision_id, diff_id=diff_id
+                )
+                return redirect(redirect_url)
+            else:
+                # TODO:  Push an error on to an error stack to show in UI
+                pass
         else:
             # TODO
             # Return validation errors
