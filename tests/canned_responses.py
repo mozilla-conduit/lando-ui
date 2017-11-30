bc4d9149a1de407d74b202b953e41329630573b4
Israel Madueme <purelogiq@gmail.com>
2017-11-30 16:38:36 -0500

    revisions: remove 'D' prefixes to match new lando-api behavior
    Summary:
    lando-api changed in D255 so that all input and output to the api
    concerning revisions will have the 'D' prefixed. This means that
    the UI was now adding an extra 'D' in places were it used or displayed
    the revision id.
    
    This patch also updates the canned responses in the tests to match the
    new expected api response.
    
    Test Plan:
    Run invoke test. You may also startup a local lando-api and
    try visiting a revision from phabricator-dev.
    
    Reviewers: davidwalsh
    
    Reviewed By: davidwalsh
    
    Differential Revision: https://phabricator.services.mozilla.com/D303

--- tests/canned_responses.py
+++ tests/canned_responses.py
@@ -3,49 +3,66 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 # yapf: disable
+# flake8: noqa
 
 LANDO_API_RESPONSE = {
     "author": {
-        "username": "dkl",
-        "url": "https://phabricator-dev.allizom.org/p/dkl/",
-        "image_url": "https://d1ro22lwo621zr.cloudfront.net/file/data/"
-                     "f7caskb6qt5gkxdkftlt/PHID-FILE-p2p6po32js7anduomkao/"
-                     "alphanumeric_aleo-white_D.png-_84c08c-0%2C0%2C0%2C0.png",
-        "real_name": "David Lawrence",
-        "phid": "PHID-USER-4wigy3sh5fc5t74vapwm"
+        "image_url": "https://d1ro22lwo621zr.cloudfront.net/file/data/6zlfwt4tc5wh5fyumbtj/PHID-FILE-olnelw7alud24uszmq55/alphanumeric_lato-dark_I.png-_b27d7a-0%2C0%2C0%2C0.3.png",
+        "phid": "PHID-USER-brksv5nhj5i2kqsr4vqs",
+        "real_name": "Israel Madueme",
+        "url": "https://phabricator-dev.allizom.org/p/imadueme/",
+        "username": "imadueme"
     },
-    "date_modified": 1508942565,
-    "status_name": "Needs Review",
-    "date_created": 1504192226,
-    "url": "https://phabricator-dev.allizom.org/D3",
-    "id": 3,
-    "bug_id": 1154784,
-    "commit_message_preview": "Bug 1154784 - test",
-    "test_plan": "test",
-    "summary": "test",
+    "bug_id": 1154786,
+    "commit_message_preview": "Bug 1154786 - Complicated UTF and binary data r=imadueme_admin,r=davidwalsh,r=smacleod,r=zalun",
+    "date_created": 1505136313,
+    "date_modified": 1511884946,
+    "diff": {
+        "authors": None,
+        "date_created": 1505136285,
+        "date_modified": 1505136313,
+        "id": 11,
+        "revision_id": "D6",
+        "vcs_base_revision": None
+    },
+    "id": "D6",
+    "parent_revisions": [],
+    "phid": "PHID-DREV-yog77ipwtobsd3ag4skx",
     "repo": {
         "full_name": "rLANDOAPI lando-api",
         "phid": "PHID-REPO-kzhprsvk5ju523p46bhg",
         "short_name": "rLANDOAPI",
         "url": "https://phabricator-dev.allizom.org/source/lando-api/"
     },
-    "parent_revisions": [],
-    "title": "test",
-    "status": 0,
-    "phid": "PHID-DREV-hv7xoyupew3yie75nmlz",
-    "diff": {
-        "authors": [
-            {
-                "email": "pzalewa@mozilla.com",
-                "name": "Piotr Zalewa"
-            }
-        ],
-        "date_created": 1506951134,
-        "date_modified": 1506951137,
-        "id": 49,
-        "revision_id": 23,
-        "vcs_base_revision": "2cb40959b19124b2de7aea328073d542e283f4c0"
-    }
+    "reviewers": [
+        {
+            "is_blocking": False,
+            "phid": "PHID-USER-jmvwye5cfm4gixkylkde",
+            "real_name": "David Walsh",
+            "status": "added",
+            "username": "davidwalsh"
+        },
+        {
+            "is_blocking": True,
+            "phid": "PHID-USER-nxwwdwtsc73zljhkhnvm",
+            "real_name": "Steven MacLeod",
+            "status": "blocking",
+            "username": "smacleod"
+        },
+        {
+            "is_blocking": False,
+            "phid": "PHID-USER-u3mluu5tvt5qcwasx3z6",
+            "real_name": "Piotr Zalewa",
+            "status": "accepted",
+            "username": "zalun"
+        }
+    ],
+    "status": 1,
+    "status_name": "Needs Revision",
+    "summary": "Test revision to see what happens",
+    "test_plan": "test",
+    "title": "Complicated UTF and binary data",
+    "url": "https://phabricator-dev.allizom.org/D6"
 }
 
 LANDO_API_LANDINGS_RESPONSE = []
