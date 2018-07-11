9e570dd2668cadc25b4dbc948315cb22e16903b4
Israel Madueme <purelogiq@gmail.com>
2018-07-11 10:38:51 -0400

    revisions: improve user experience of landing (Bug 1473795)
    Summary:
    Prior to this commit, all the warnings and blockers would appear
    on page load; e.g. when users just loaded the page and didn't even
    intend on landing it, when they are viewing a revision that already
    landed in the past, and so on.
    
    Rather than give users this info when they may not need it, we now
    display the warnings and blockers only after the user clicks the land
    button. This gives the user context - that these messages are related
    to their attempt to land the revision.
    
    They can click the land button again to follow through with landing
    (assuming no blockers, warnings are checked, etc.)
    
    Test Plan: Manually test with some revisions. Video in the bug.
    
    Reviewers: smacleod, mars
    
    Reviewed By: mars
    
    Subscribers: mars
    
    Bug #: 1473795
    
    Differential Revision: https://phabricator.services.mozilla.com/D2070

--- landoui/assets_src/js/components/Revision.js
+++ landoui/assets_src/js/components/Revision.js
@@ -3,7 +3,66 @@
 $.fn.revision = function() {
   return this.each(function() {
     let $revision = $(this);
-    let $landButton = $revision.find('.RevisionPage-actions button');
+    let $landButton = $revision.find('.RevisionPage-land-button');
+    let $landInfo = $revision.find('.RevisionPage-landing-info');
+    let $blockers = $revision.find('.RevisionPage-blockers');
+    let $warnings = $revision.find('.RevisionPage-warnings input[type=checkbox]');
+
+    let setLandButtonState = (title, subtitle, disabled, inprogress) => {
+      $landButton.attr({'disabled': disabled});
+      if(inprogress) $landButton.attr({'inprogress': true});
+      if(title) $landButton.find('.RevisionPage-actions-headline').text(title);
+      if(subtitle) $landButton.find('.RevisionPage-actions-subtitle').text(subtitle);
+    };
+
+    let landingTransition = (e) => {
+      e.preventDefault();
+      $landButton.unbind('click', landingTransition);
+
+      // Show warnings and blockers and disable button until animation finishes.
+      setLandButtonState(null, null, true, false);
+      $landInfo.show(400, transitionFinish);
+    };
+
+    let transitionFinish = () => {
+      let initialTitle = $landButton.find('.RevisionPage-actions-headline').text();
+
+      // Update land button state
+      if($blockers.length) {
+        setLandButtonState('Landing is blocked', ' ', true, false);
+      }
+      else if ($warnings.length) {
+        setLandButtonState(initialTitle, 'Check off each warning to land', true, false);
+      }
+      else {
+        setLandButtonState('Confirm', 'Ready to Land', false, false);
+      }
+
+      // Enable and disable buttons depending on if all warning checkboxes
+      // (if any) are checked or not.
+      $warnings.on('change', () => {
+        let checked = $warnings.filter(function() {
+          return this.checked;
+        });
+
+        if(checked.length !== $warnings.length) {
+          setLandButtonState(initialTitle, 'Check off each warning to land', true, false);
+        }
+        else {
+          setLandButtonState('Confirm', 'Ready to Land', false, false);
+        }
+      });
+
+      // Disable landing button and submit form upon click
+      $landButton.on('click', function(e) {
+        e.preventDefault();
+        if($landButton.attr('disabled')) {
+          return;
+        }
+        setLandButtonState('Landing Queuing...', 'Please wait', true, true);
+        $revision.find(".RevisionPage-form").submit();
+      });
+    };
 
     // Format timestamps
     $revision.find('time[data-timestamp]').formatTime();
@@ -13,32 +72,7 @@ $.fn.revision = function() {
       $revision.find('.RevisionPage-commit-full').slideToggle();
     });
 
-    // Disable landing button upon click
-    $landButton.on('click', function() {
-      if($landButton.attr('disabled')) {
-        return;
-      }
-
-      $landButton.attr({'disabled': true, 'inprogress': true});
-      $landButton.find('.RevisionPage-actions-headline').html('Landing Queuing...');
-      $landButton.find('.RevisionPage-actions-subtitle').html('Please wait');
-    });
-
-    // Enable and disable buttons depending on if all warning checkboxes (if any)
-    // are checked or not
-    let $warnings = $revision.find('.RevisionPage-warnings input[type=checkbox]');
-    $warnings.on('change', function() {
-      let checked = $warnings.filter(function() {
-        return this.checked;
-      });
-      
-      if(checked.length !== $warnings.length) {
-        $landButton.attr('disabled', true);
-      }
-      else {
-        $landButton.removeAttr('disabled');
-      }
-    });
-
+    // Transition to attempt a landing on initial click
+    $landButton.bind('click', landingTransition);
   });
 };
