0f966cccbdf1a38adf97dfba4ddca468ead8a9c8
Israel Madueme <purelogiq@gmail.com>
2018-07-23 13:23:48 -0400

    navbar: specify default gravatar icon (Bug 1455361)
    Summary:
    If a user doesn't have a gravatar image for their auth0 email address,
    gravatar uses auth0's provided default which redirects to
    *.wp.com/cdn.auth0.com/. Instead of adding *.wp.com to our
    CSP, this commit lets gravatar itself provide us with a default 'identicon'
    image (examples here: https://en.gravatar.com/site/implement/images/)
    
    Test Plan: Check the image url while logged in. Run invoke test.
    
    Reviewers: smacleod
    
    Reviewed By: smacleod
    
    Subscribers: smacleod
    
    Bug #: 1455361
    
    Differential Revision: https://phabricator.services.mozilla.com/D2126

--- landoui/template_helpers.py
+++ landoui/template_helpers.py
@@ -1,16 +1,26 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-from flask import (Blueprint)
+import logging
+import re
+import urllib.parse
+
+from flask import (Blueprint, current_app, escape)
 
 from landoui import helpers
 
+logger = logging.getLogger(__name__)
 template_helpers = Blueprint('template_helpers', __name__)
 
 
 @template_helpers.app_template_global()
 def is_user_authenticated():
     return helpers.is_user_authenticated()
+
+
+@template_helpers.app_template_filter()
+def escape_html(text):
+    return escape(text)
 
 
 @template_helpers.app_template_filter()
@@ -55,3 +65,72 @@ def latest_status(statuses):
 
     statuses = sorted(statuses, key=lambda k: k['updated_at'], reverse=True)
     return statuses[0]['status']
+
+
+@template_helpers.app_template_filter()
+def avatar_url(url):
+    # If a user doesn't have a gravatar image for their auth0 email address,
+    # gravatar uses auth0's provided default which redirects to
+    # *.wp.com/cdn.auth0.com/. Instead of whitelisting this in our CSP,
+    # here, we opt into a default generated image provided by gravatar.
+    try:
+        parsed_url = urllib.parse.urlsplit(url)
+        if not parsed_url.netloc:
+            raise ValueError('Avatar URLs should not be relative')
+    except (AttributeError, ValueError):
+        logger.debug('Invalid avatar url provided', extra={'url': url})
+        return ''
+
+    if parsed_url.netloc in ('s.gravatar.com', 'www.gravatar.com'):
+        query = urllib.parse.parse_qs(parsed_url.query)
+        query['d'] = 'identicon'
+        parsed_url = (
+            parsed_url[:3] +
+            (urllib.parse.urlencode(query, doseq=True), ) + parsed_url[4:]
+        )
+
+    return urllib.parse.urlunsplit(parsed_url)
+
+
+@template_helpers.app_template_filter()
+def linkify_bug_numbers(text):
+    search = r'(?=\b)(Bug (\d+))(?=\b)'
+    replace = '<a href="{bmo_url}/show_bug.cgi?id=\g<2>">\g<1></a>'.format(
+        bmo_url=current_app.config['BUGZILLA_URL']
+    )
+    return re.sub(search, replace, str(text), flags=re.IGNORECASE)
+
+
+@template_helpers.app_template_filter()
+def linkify_revision_urls(text):
+    search = (
+        r'(?=\b)(' + re.escape(current_app.config['PHABRICATOR_URL']) +
+        r'/D\d+)(?=\b)'
+    )
+    replace = '<a href="\g<1>">\g<1></a>'
+    return re.sub(search, replace, str(text), flags=re.IGNORECASE)
+
+
+@template_helpers.app_template_filter()
+def linkify_diff_ids(text, revision_id):
+    search = r'(?=\b)(Diff (\d+))(?=\b)'
+    replace = '<a href="{phab_url}/{revision_id}?id=\g<2>">\g<1></a>'.format(
+        phab_url=current_app.config['PHABRICATOR_URL'],
+        revision_id=revision_id
+    )
+    return re.sub(search, replace, str(text), flags=re.IGNORECASE)
+
+
+@template_helpers.app_template_filter()
+def linkify_commit_id(text, landing_status):
+    # The landing status result is not always guaranteed to be a commit id. It
+    # can be a message saying that the landing was queued and will land later.
+    if landing_status['status'] != 'landed':
+        return text
+
+    commit_id = landing_status['result']
+    search = r'(?=\b)(' + re.escape(commit_id) + r')(?=\b)'
+    replace = '<a href="{tree_url}/rev/\g<1>">{tree_url}/rev/\g<1></a>'.format(
+        tree_url=landing_status['tree_url']
+    )
+    return re.sub(search, replace, str(text))  # This is case sensitive
