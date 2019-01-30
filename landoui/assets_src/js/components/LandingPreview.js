fc0a4ce651608b261150aeadf33d0d83cc2796ab
nikkis <nikkisharpley@gmail.com>
2019-01-30 16:08:53 -0500

    landing preview: separate toggleAll button into expand and collapse (Bug 1509515).
    Summary:
    Further to the last fix for Bug 1500223, which removed the max-height
    limit and overflow properties; this fix improves the UI with the
    addition of buttons to toggle the expansion and contraction of
    individual, or all, long commit messages.
    
    Below each commit message, the number of lines that are hidden is
    displayed. A separate expandAllButton and collapseAllButton for all
    commit messages provides a neat solution in which the user can display
    or hide all long messages. Alternatively, each long message has its
    own expand/collapse button with which the user can toggle individually
    as they please.
    
    Depends on D14996.
    
    Test Plan: Manually tested in browser.
    
    Reviewers: smacleod
    
    Reviewed By: smacleod
    
    Bug #: 1509515
    
    Differential Revision: https://phabricator.services.mozilla.com/D15000

--- landoui/assets_src/js/components/LandingPreview.js
+++ landoui/assets_src/js/components/LandingPreview.js
@@ -7,11 +7,14 @@ $.fn.landingPreview = function() {
     let $warnings = $landingPreview.find('.StackPage-landingPreview-warnings input[type=checkbox]');
     let $blocker = $landingPreview.find('.StackPage-landingPreview-blocker');
     let $landButton = $landingPreview.find('.StackPage-landingPreview-land');
+    let $revisions = $landingPreview.find('.StackPage-landingPreview-revision');
+    let $expandAllButton = $landingPreview.find('.StackPage-landingPreview-expandAll');
+    let $collapseAllButton = $landingPreview.find('.StackPage-landingPreview-collapseAll');
 
     // Reach outside my component, because I'm a pragmatist.
     let $previewButton = $('.StackPage-preview-button');
 
-    let calculateButtonState = () => {
+    let calculateLandButtonState = () => {
       if($blocker.length > 0) {
         $landButton.attr({'disabled': true});
         $landButton.text('Landing is blocked');
@@ -30,13 +33,80 @@ $.fn.landingPreview = function() {
       }
     };
 
+
     $landButton.on('click', () => {
       $landButton.attr({'disabled': true});
       $landButton.text('Landing in progress...');
     });
+
+    let expandCommitMessage = ($commitMessage, $seeMore, $toggleButton, lines) => {
+      $commitMessage.css('max-height', 'none');
+      $commitMessage.data('expanded', true);
+      $toggleButton.text('Hide lines');
+      $seeMore.css('display', 'none');
+    };
+
+    let collapseCommitMessage = ($commitMessage, $seeMore, $toggleButton, lines) => {
+      $commitMessage.css('max-height', '');
+      $commitMessage.data('expanded', false);
+      $toggleButton.text('Show all ' + lines + ' lines');
+      $seeMore.css('display', 'block');
+    };
+
+    let toggleCommitMessage = ($commitMessage, $seeMore, $toggleButton, lines) => {
+      if($commitMessage.data('expanded')) {
+        collapseCommitMessage($commitMessage, $seeMore, $toggleButton, lines);
+      } else {
+        expandCommitMessage($commitMessage, $seeMore, $toggleButton, lines);
+      }
+    };
+
+    let longMessages = 0;
+
+    $revisions.each(function () {
+      let $revision = $(this);
+      let $toggleButton = $revision.find('.StackPage-landingPreview-expand');
+      let $commitMessage = $revision.find('.StackPage-landingPreview-commitMessage');
+      let $seeMore = $revision.find('.StackPage-landingPreview-seeMore');
+      let lines = $commitMessage.text().split(/\r\n|\r|\n/).length;
+
+      if (lines <= 5){
+        $toggleButton.hide();
+        return;
+      }
+
+      longMessages++;
+
+      // Sets up the display of how many lines are hidden:
+      // expandCommitMessage and collapseCommitMessage merely toggle this when clicked.
+      $toggleButton.text('Show all ' + lines + ' lines');
+      $seeMore.text('... (' + (lines - 5) + ' more lines)');
+
+      $toggleButton.on('click', (e) => {
+        e.preventDefault();
+        toggleCommitMessage($commitMessage, $seeMore, $toggleButton, lines);
+      });
+
+      $expandAllButton.on('click', (e) => {
+        e.preventDefault();
+        expandCommitMessage($commitMessage, $seeMore, $toggleButton, lines);
+      });
+
+      $collapseAllButton.on('click', (e) => {
+        e.preventDefault();
+        collapseCommitMessage($commitMessage, $seeMore, $toggleButton, lines)
+      });
+    });
+
+    if ($revisions.length === 1 || longMessages === 0) {
+      $expandAllButton.css('display', 'none');
+      $collapseAllButton.css('display', 'none');
+    }
+
+
     $previewButton.on('click', (e) => {
       e.preventDefault();
-      calculateButtonState();
+      calculateLandButtonState();
       $landingPreview.show();
     });
     $close.on('click', (e) => {
@@ -44,7 +114,7 @@ $.fn.landingPreview = function() {
       $landingPreview.hide();
     });
     $warnings.on('change', () => {
-      calculateButtonState();
+      calculateLandButtonState();
     });
   });
 };
