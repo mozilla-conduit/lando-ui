11f827d724ae2938a966f3696687b20910aefac1
Connor Sheehan <cosheehan@mozilla.com>
2022-11-01 12:05:06 -0400

    landoapi: raise `requests` exception instead of `JSONDecodeError`
    Sentry is categorizing any unexpected response as a `JSONDecodeError`
    because those responses are typically returning HTML webpages or empty
    response bodies. Catch the JSONDecodeError early and attempt to raise
    the more detailed exception from `requests` if possible.

--- landoui/landoapi.py
+++ landoui/landoapi.py
@@ -69,7 +69,14 @@ class LandoAPI:
                 },
             )
 
-            data = response.json()
+            try:
+                data = response.json()
+            except JSONDecodeError as exc:
+                # Raise the error from `requests` if the status was unexpected.
+                response.raise_for_status()
+
+                # Raise the JSONDecodeError if the status was okay.
+                raise exc
         except requests.RequestException as exc:
             raise LandoAPICommunicationException(
                 "An error occurred when communicating with Lando API"
