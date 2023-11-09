86b95ab8a8584c2ed3a7e8dd1d6b7bf70b44247c
Connor Sheehan <cosheehan@mozilla.com>
2023-11-08 20:21:02 -0500

    remove dev TODO

--- landoui/template_helpers.py
+++ landoui/template_helpers.py
@@ -32,12 +32,6 @@ def is_user_authenticated() -> bool:
     return helpers.is_user_authenticated()
 
 
-# TODO remove this
-@template_helpers.app_template_global()
-def is_user_authenticated_TODO() -> bool:
-    return helpers.is_user_authenticated_TODO()
-
-
 @template_helpers.app_template_global()
 def user_has_phabricator_token() -> bool:
     return helpers.get_phabricator_api_token() is not None
