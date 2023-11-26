1bc0f57f518571836b43c2cce4e511bf68004cb6
Connor Sheehan <cosheehan@mozilla.com>
2023-11-26 12:08:44 -0500

    add a test file

---
+++ tests/test_treestatus.py
@@ -0,0 +1,30 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+def test_build_recent_changes_stack():
+    pass
+
+
+def test_build_update_json_body():
+    pass
+
+
+def test_update_form_validate_trees():
+    pass
+
+
+def test_update_form_validate_reason():
+    pass
+
+
+def test_update_form_validate_reason_category():
+    pass
+
+
+def test_update_form_to_submitted_json():
+    pass
+
+
+def test_recent_changes_action():
+    pass
